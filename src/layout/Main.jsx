5be246f9633880201ed0e2990cde4699464bbc6b
Uzzal Bhowmik <ubcreativity111@gmail.com>
2023-06-18 15:25:48 +0600

    favicon and title updated

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
